6985c61c4355a457ad064e7b6c767bf618edb221
Nicolas <nicolas.lagoutte@connected-ventures.com>
2018-02-01 14:47:21 +0000

    Implement the first version

--- README.md
+++ README.md
@@ -4,4 +4,46 @@
 [![Coverage Status](https://coveralls.io/repos/github/fresh8/prom-client-timer-unit/badge.svg?branch=master)](https://coveralls.io/github/fresh8/prom-client-timer-unit?branch=master)
 [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 
-A helper function to help record time in other units than seconds
+A helper function to help record time in other units than seconds. 
+It really just divides the time measurment (in seconds) by whatever number you give it.
+
+## Usage
+
+### At a glance
+#### Javascript
+```js
+const myMetric = require('prom-client').Histogram()
+const startTimer = require('@fresh8/prom-client-timer-unit')
+
+const stopTimer = startTimer(myMetric, 1e-3)
+// [...] your code doing things
+stopTimer() //will calculate the time elapsed and register it in `myMetric`
+```
+
+#### Typescript
+```ts
+import * as promClient from 'prom-client'
+import * as startTimer from '@fresh8/prom-client-timer-unit'
+
+const myMetric = promClient.Histogram()
+const stopTimer = startTimer(myMetric, 1e-3)
+// [...] your code doing things
+stopTimer() //will calculate the time elapsed and register it in `myMetric`
+```
+
+### API
+#### startTimer(metric, factor, labels)
+* **metric** : your metric straight from `prom-client` (e.g: `new promClient.Histogram(opts)`)
+* **factor** (optional): The factor to be applied to the observed number of seconds. Examples: `1e-3` for milliseconds, `60` for minutes. Defaults to `1`
+* **labels** (optional): labels to apply to the observation. This is to follow the same signature as the original `startTimer` function in `prom-client`. Defaults to `{}`
+* returns: stopTimer
+
+Start counting time using `process.hrtime`.
+
+#### stopTimer(labels)
+* **labels** : labels to apply to the observation. They will be merged with the labels given to `startTimer` (if any). Defaults to `{}`
+
+Stops the timer and record the elapsed time in the metric given to `startTimer`, dividing the default time (in seconds) by the provided `factor`.
+
+## Credits
+Implementation very heavily inspired from the [original in prom-client](https://github.com/siimon/prom-client/blob/4c5b6c73ca5bea6889e77f61ef4b99b5e0250d7a/lib/summary.js#L180)
--- test.js
+++ test.js
@@ -1,9 +1,86 @@
-const { assert } = require('chai')
+const { assert, expect } = require('chai')
 
-const index = require('./index')
+const startTimer = require('./index')
 
-describe('Index', () => {
+class MetricMock {
+  constructor() {
+    this.startTimer = 'truthy'
+  }
+
+  observe(...args) {
+    this.observed = args
+  }
+}
+
+describe('startTimer', () => {
   it('Is defined', () => {
-    assert.isDefined(index)
+    assert.isDefined(startTimer)
+  })
+
+  it('returns a function', () => {
+    expect(startTimer).to.be.a('function')
+  })
+
+  it('throws when missing arguments', () => {
+    expect(startTimer).to.throw('Invalid metric')
+  })
+
+  it('throws when first arguments is invalid', () => {
+    expect(startTimer).to.throw('Invalid metric')
+    expect(startTimer.bind(null, {})).to.throw('Invalid metric')
+    expect(startTimer.bind(null, { startTimer: 'truthy' })).to.throw(
+      'Invalid metric'
+    )
+    expect(startTimer.bind(null, { observe: 'truthy' })).to.throw(
+      'Invalid metric'
+    )
+  })
+
+  describe('stop', () => {
+    it('is a function', () => {
+      const stop = startTimer(new MetricMock())
+      expect(stop).to.be.a('function')
+    })
+
+    it('observe elapsed time', () => {
+      const metric = new MetricMock()
+      const stop = startTimer(metric)
+      stop()
+      assert.isDefined(metric.observed)
+      expect(metric.observed.length).to.equal(2)
+      expect(metric.observed[1]).to.be.greaterThan(0)
+    })
+
+    it('defaults factor to seconds', done => {
+      const metric = new MetricMock()
+      const stop = startTimer(metric)
+      setTimeout(() => {
+        stop()
+        expect(metric.observed[1])
+          .to.be.above(0.28)
+          .and.below(0.5)
+        done()
+      }, 300)
+    })
+
+    it('applies custom factors', done => {
+      //test in milliseconds
+      const metric = new MetricMock()
+      const stop = startTimer(metric, 1e-3)
+      setTimeout(() => {
+        stop()
+        expect(metric.observed[1])
+          .to.be.above(280)
+          .and.below(500)
+        done()
+      }, 300)
+    })
+
+    it('applies both start and end labels', () => {
+      const metric = new MetricMock()
+      const stop = startTimer(metric, null, { start: 'yup' })
+      stop({ end: 'already' })
+      expect(metric.observed[0]).to.deep.equal({ start: 'yup', end: 'already' })
+    })
   })
 })
